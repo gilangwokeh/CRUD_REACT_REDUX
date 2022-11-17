80e5881fdbaba8e8157142411f752874b647e3f1
gilangwokeh <gilangwokeh12@gmail.com>
2022-11-17 18:49:30 +0700

    update code

--- src/feutures/users/userSlide.js
+++ src/feutures/users/userSlide.js
@@ -1,36 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 const initialState = [
-  { id: '1', name: 'amanda', umur: '22', },
-  { id: '2', name: 'john', umur: '19',},
+  JSON.parse(localStorage.getItem('form'))
 ]
 
 
 const userSlice = createSlice({
   name: 'users',
-  initialState ,
+  initialState,
   reducers: {
-    addUser: (state,action) => {
+    addUser: (state, action) => {
       state.push(action.payload)
     },
-    editUser: (state,action) => {
-      const {id , name ,umur,hobi,Alamat } = action.payload;
-      const existUser = state.find(user => user.id ===  id);
-      if (existUser){
+    editUser: (state, action) => {
+      const { id, name, umur, hobi, Alamat } = action.payload;
+      const existUser = state.find(user => user.id === id);
+      if (existUser) {
         existUser.name = name;
-        existUser.umur =  umur;
-        existUser.hobi =  hobi;
-        existUser.Alamat =  Alamat;
+        existUser.umur = umur;
+        existUser.hobi = hobi;
+        existUser.Alamat = Alamat;
       }
     },
-    deleteUser : (state,action) => {
+    deleteUser: (state, action) => {
       const { id } = action.payload;
-      const existUser = state.find(user => user.id ===  id);
-      if (existUser){
-        return state.filter(user => user.id  !== id)
+      const existUser = state.find(user => user.id === id);
+      if (existUser) {
+        return state.filter(user => user.id !== id)
       }
     }
   }
 })
 
-export const { addUser ,editUser ,deleteUser} = userSlice.actions;
+export const { addUser, editUser, deleteUser } = userSlice.actions;
 export default userSlice.reducer;
